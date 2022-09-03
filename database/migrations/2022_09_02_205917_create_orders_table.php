c15a3389a239aeca13854f07be79557cd271315f
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2022-09-03 02:35:46 +0200

    add laravel admin

---
+++ database/migrations/2022_09_02_205917_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('date');
+            $table->string('cake_size');
+            $table->string('cupcakes');
+            $table->string('flavour');
+            $table->string('fillings');
+            $table->string('toppings');
+            $table->string('theme');
+            $table->string('food_allergies');
+            $table->string('delivery');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
